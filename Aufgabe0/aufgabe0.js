ffaa670c20280499d88a303c07d5b3dd7e1473d4
Alistone <Alistone@Alistone-PC.fritz.box>
2017-03-19 17:45:25 +0100

    aufgabe0

---
+++ Aufgabe0/aufgabe0.js
@@ -0,0 +1,16 @@
+/*
+Aufgabe:    Aufgabe 0
+Name:       Alisia Steiner
+Matrikel:   254788
+Datum:      18.03.2017
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    var getName = prompt("Huhu! Wie heißt du?");
+    document.getElementById("Hello").innerHTML = "Hallo " + getName + "! Ich danke für deinen Besuch :)";
+    ;
+});
+//# sourceMappingURL=aufgabe0.js.map
